65b2bf433d5646187c9c6690010080a05a57a517
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2020-01-31 15:03:34 -0600

    try 1

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
